2bd707de9f270ab8f4af262ae48ec768e1ef46cb
thecloud20 <pratikslondhe20@gmail.com>
2026-02-19 18:46:18 +0530

    EXTRACTED LAYER COMPLETED NOW NEXT TRANSFORM AND LOAD

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.extract.extractor import run_extraction
+
+if __name__=="__main__":
+    run_extraction()
---
+++ src/utils/config.py
@@ -0,0 +1,13 @@
+ORDERS_PATH = "data/raw/orders.csv"
+PRODUCTS_PATH = "data/raw/products.json"
+CUSTOMERS_PATH = "data/raw/customersc.csv"
+
+STAGING_ORDERS = "data/staging/orders"
+STAGING_PRODUCTS = "data/staging/products"
+STAGING_CUSTOMERS = "data/staging/customers"
+
+OUTPUT_ENRICHED = "output/enriched"
+
+OUTPUT_SUMMARY = "output/summary"
+
+OUTPUT_REPORT_CSV = "output/report"
